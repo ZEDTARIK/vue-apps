9f45274c0db01c166c03ca5d42b8203679d1edc5
zouhair.ettarak <ettarak.zouhair@gmail.com>
2023-03-11 11:36:52 +0100

    Step configuration router

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import EventsView from "../views/EventsView.vue";
+import AboutView from "../views/AboutView";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "Events",
+    component: EventsView,
   },
   {
     path: "/about",
     name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: AboutView,
   },
 ];
 
---
+++ src/views/EventsView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="events">
+    <h4>List Events</h4>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+
+export default {
+  name: "EventsView",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 51547,
+          title: "Smet consectetur adipisicing",
+          decription:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, inventore.",
+          time: "10:00",
+          date: "10 March 2023",
+        },
+        {
+          id: 52367,
+          title: "sit elit invetor",
+          decription:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, inventore.",
+          time: "10:00",
+          date: "26 July 2021",
+        },
+        {
+          id: 74515,
+          title: "Lorem ipsum dolor sit amet consectetur.",
+          decription:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, inventore.",
+          time: "15:30",
+          date: "86 September 2022",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
